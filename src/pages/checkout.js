d0271bd7669cb388dbd6ca5d8b955de38bd7c5ef
Muhammad Nabeel <m.nabeeel96@gmail.com>
2021-06-14 01:09:51 +0500

    stripe checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,12 +5,31 @@ import CheckoutProduct from '../components/CheckoutProduct';
 import Header from '../components/Header';
 import { selectItems, selectTotal } from '../slices/basketSlice';
 import Currency from 'react-currency-formatter';
+import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const [session] = useSession();
   const total = useSelector(selectTotal);
 
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    // call the backend to create a checkout session...
+    const checkoutSession = await axios.post('/api/create-checkout-session', {
+      items: items,
+      email: session.user.email,
+    });
+
+    // Redirect user/custom to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
   return (
     <div>
       <Header />
@@ -61,6 +80,8 @@ function Checkout() {
               </h2>
 
               <button
+                role='link'
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
